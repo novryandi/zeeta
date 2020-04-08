d2323b2a17ea4b92c42d7692583af41a1529bf8f
Abbi Kurnia <requiemzerophantasm@gmail.com>
2020-04-08 15:49:37 +0700

    Memperbaiki gambar yg strech dan menambahkan gambar klinik

--- application/views/produk/produk1.php
+++ application/views/produk/produk1.php
@@ -1,10 +1,11 @@
-<h1>produk1</h1>
+<!-- //kalo bisa otomatis dari backend -->
+<h1>Produk 1</h1>
 <div class="container" style="margin: 75px;">
 			<div class="row">
 				<div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto">
 					<!-- block1 -->
 					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img width="120px" height="300px" src="<?= base_url(); ?>assets/template/images/serum-purple1.jpg "alt="IMG-BENNER"> 
+						<img width="auto" height="380px" src="<?= base_url(); ?>assets/template/images/serum-purple1.jpg "alt="IMG-BENNER"> 
 					</div>
 				</div>
 
@@ -15,6 +16,7 @@
 						<p>Asam Tranexamat & Vitamin C
 						Berfungsi sebagai Anti Oksidant dam Mencerahkan wajah .</p>
 						</br>
+						<h2 class="harga">Rp.75.000,-</h2>
 					<input type= "button" href="<?= base_url('reservasi');?>" class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4"  style="color: gold;" value="Tambahkan Ke Reservasi">
 					</input>	
 					</div>
