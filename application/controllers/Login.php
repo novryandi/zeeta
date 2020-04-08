47f6a2dc0587d2d08503d3b178b67361d25401f1
Abbi Kurnia <requiemzerophantasm@gmail.com>
2020-04-08 22:09:00 +0700

    Login - UI

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,8 +5,12 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		$data = array( 'title' => 'Login Adminisrator');
-		$this->load->view('login/list', $data);
+		$data =array( 'title'	=>	'Zeta Salon',
+						'isi'	=>	'home/list');
+
+		$this->load->view('template/v_header');
+		$this->load->view('login/login', $data);
+		$this->load->view('template/v_footer');
 		
 	}
 
