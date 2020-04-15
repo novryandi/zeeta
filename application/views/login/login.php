2c3ded0439729be474f820466f56df12875a14dd
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-04-16 03:07:46 +0700

    Update login.php

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -8,21 +8,26 @@
 						<img  src="<?= base_url('assets/template/images/zeeta.png'); ?>" alt="IMG-LOGO">
 					</a>
 				</div>
+
+				<?= $this->session->flashdata('message');?>
+
 				<div class="card-body">
-					<form>
+					<form class="user" method="post" action="<?= base_url('Login')?>">
+
 						<div class="input-group form-group">
 							<div class="input-group-prepend">
 								<span class="input-group-text"><i class="fas fa-user"></i></span>
 							</div>
-							<input type="text" class="form-control" placeholder="username">
-							
+							<input type="text" class="form-control" placeholder="username" name="username" id="username">
 						</div>
+
 						<div class="input-group form-group">
 							<div class="input-group-prepend">
 								<span class="input-group-text"><i class="fas fa-key"></i></span>
 							</div>
-							<input type="password" class="form-control" placeholder="password">
+							<input type="password" class="form-control" placeholder="password" name="password" id="password">
 						</div>
+
 						<div class="form-group">
 							<input type="submit" value="Login" class="btn float-right login_btn">
 						</div>
