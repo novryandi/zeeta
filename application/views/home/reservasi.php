1611af669d54d2df5c7988700c9f93b71c643c72
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-04-16 04:16:25 +0700

    nambahin fungsi form

--- application/views/home/reservasi.php
+++ application/views/home/reservasi.php
@@ -1,6 +1,6 @@
 <h1>Reservasi</h1>
 
-
+<form method="post" action="<?= base_url('Homepage/reservasi')?>">
 <div class="container">
    <div class="panel panel-primary">
       <div class="panel-body">
@@ -8,7 +8,7 @@
             <div class="col-md-12">
                <div class="form-group">
                   <label class="control-label">Nama</label>
-                  <input type="text" class="form-control" name="name" id="name">
+                  <input type="text" class="form-control" name="nama" id="nama">
                </div>
             </div>
          </div>
@@ -56,7 +56,7 @@
 
                     <!-- Database -->
                        <!--  require('db_config.php');
-                        $sql = "SELECT * FROM demo_state"; 
+                        $sql = "SELECT * FROM demo_state";
                         $result = $mysqli->query($sql);
                         while($row = $result->fetch_assoc()){
                             echo "<option value='".$row['id']."'>".$row['name']."</option>";
@@ -89,10 +89,22 @@ $( "select[name='state']" ).change(function () {
     }
 });
 </script> -->
+
+        <div class="row">
+           <div class="col-md-12">
+              <div class="form-group">
+                 <label class="control-label">Jumlah</label>
+                 <input type="text" class="form-control" name="jumlah" id="jumlah">
+              </div>
+           </div>
+        </div>
+
          <input type="submit" class="btn btn-primary" value="Submit">
       </div>
    </div>
 </div>
+
+</form>
 
 <script type='text/javascript'>
 				$('#datetimepicker1').datetimepicker({
@@ -100,9 +112,9 @@ $( "select[name='state']" ).change(function () {
 				});
 </script>
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script> 
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
 
 
