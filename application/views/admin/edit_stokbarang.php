da8b743bf0c0995edd8a4afcf335513732b258a8
Abbi Kurnia <requiemzerophantasm@gmail.com>
2020-04-27 10:06:28 +0700

    update edit stok barang

--- application/views/admin/edit_stokbarang.php
+++ application/views/admin/edit_stokbarang.php
@@ -16,26 +16,6 @@
           <div class="box box-primary">
             <!-- /.box-header -->
             <!-- form start -->
-            <form role="form" method="post" action="#">
-              <div class="box-body">
-                <div class="form-group">
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-      Edit Barang
-      </h1>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <!-- left column -->
-        <div class="col-md-12">
-          <!-- general form elements -->
-          <div class="box box-primary">
-            <!-- /.box-header -->
-            <!-- form start -->
             <form role="form" method="post" action="<?= base_url()?>Barang/Edit/<?= $produk['id_produk'];?>">
               <div class="box-body">
                 <div class="form-group">
@@ -55,39 +35,6 @@
               <!-- /.box-body -->
               <div class="box-footer">
                 <button type="submit" class="btn btn-primary" title="Ubah Data"> <i class="glyphicon glyphicon-floppy-disk"></i> Simpan</button>
-              </div>
-            </form>
-          </div>
-          <!-- /.box -->
-        </div>
-      </div>
-    </section>
-    <!-- /.content -->
-  </div>
-<!-- /.content-wrapper -->
-
-                  <label>Kode Barang</label>
-                  <input type="text" name="kode" class="form-control" placeholder="Kode Barang" required>
-                </div>
-                <div class="form-group">
-                  <label>Nama</label>
-                  <input type="text" name="nama" class="form-control" placeholder="Nama" required>
-                </div>
-                <div class="form-group">
-                  <label>Jumlah stok</label>
-                  <input type="text" name="jumlah" class="form-control" placeholder="Jumlah Stok" required>
-                </div>
-
-                <div class="form-group">
-                  <label>Foto barang</label>
-                  <input type="file" name="filefoto" class="dropify" data-height="300">
-                </div>
-                
-                
-              </div>
-              <!-- /.box-body -->
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary" title="Simpan Data"> <i class="glyphicon glyphicon-floppy-disk"></i> Simpan</button>
               </div>
             </form>
           </div>
