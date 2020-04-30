e58744cb8aa53ed6e3d4be466e273f847d9f1dda
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-04-30 23:40:24 +0700

    Update edit_pengguna.php

--- application/views/admin/edit_pengguna.php
+++ application/views/admin/edit_pengguna.php
@@ -16,30 +16,33 @@
           <div class="box box-primary">
             <!-- /.box-header -->
             <!-- form start -->
-            <form autocomplete="off" role="form" method="post" action="#">
+            <form enctype="multipart/form-data" method="post" action="<?= base_url(); ?>Profil/edit/<?= $admin['id_admin'];?>">
               <div class="box-body">
-                <div class="form-group">
-                  <label>Foto Profil</label>
-                  <input type="file" name="filefoto" class="dropify" data-height="300">
-                </div>
-            
+                <?php if(validation_errors()) :?>
+                  <div class="alert alert-danger">
+                    <?= validation_errors()?>
+                  </div>
+                <?php endif;?>
+
                 <div class="form-group">
                   <label>Nama</label>
-                  <input type="text" name="nama" class="form-control" placeholder="Nama" required>
+                  <input type="text" name="nama_admin" id="nama_admin" class="form-control" value="<?= $admin['nama_admin'];?>" required>
                 </div>
 
                 <div class="form-group">
-                  <label>Username</label>
-                  <input type="text" name="username" class="form-control" placeholder="username" required>
+                  <label>New Password</label>
+                  <input type="password" name="password" id="password" class="form-control" placeholder="password" required>
                 </div>
 
                 <div class="form-group">
-                  <label>Password</label>
-                  <input type="password" name="password" class="form-control" placeholder="password" required>
-                  <input type="checkbox" onclick="">Show Password 
+                  <label>Re-type Password</label>
+                  <input type="password" name="password1" id="password1" class="form-control" placeholder="password" required>
                 </div>
 
-        				
+                <div class="form-group">
+                  <label>Foto Profil</label>
+                  <input type="file" name="foto_admin" id="foto_admin" class="dropify" data-height="300">
+                </div>
 
               <!-- /.box-body -->
               <div class="box-footer">
