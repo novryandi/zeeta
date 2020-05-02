66b6107fcf2f7d6620fe0a53dbc95923fe807162
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-03 06:51:07 +0700

    Update AdminHome.php

--- application/controllers/AdminHome.php
+++ application/controllers/AdminHome.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class AdminHome extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Keamanan');
+		$this->load->model('Jasa');
+	}
+
 	public function index()
 	{
 		$isi['title'] = 'Klinik';
@@ -21,9 +27,11 @@ class AdminHome extends CI_Controller {
 
 	public function home_jasa()
 	{
+		$this->Keamanan->getKeamananAdm();
+		$data['jasa'] = $this->Jasa->getAllJasa();
 		$isi['title'] = 'Klinik';
 		$this->load->view('admin/header', $isi);
-		$this->load->view('admin/jasaHome');
+		$this->load->view('admin/jasaHome',$data);
 		$this->load->view('admin/footer');
 	}
 }
