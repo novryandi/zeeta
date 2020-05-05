6a15635e75f1c7ec1857e3c6fa7349035b168a5d
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-06 03:09:24 +0700

    Update jasa.php

--- application/views/home/jasa.php
+++ application/views/home/jasa.php
@@ -6,152 +6,32 @@
 				</h3>
 			</div>
 		<div class="container">
-			
+
 			<div class="row">
 				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
 					<!-- block1 -->
+					<?php foreach($jasa as $js) :?>
 					<div class="block2">
 							<div class="block2-img hov-img-zoom wrap-pic-w of-hidden pos-relative">
-								<img src="<?= base_url(); ?>assets/template/images/serum-purple1.jpg" alt="IMG-PRODUCT">
+								<img src="<?= base_url(); ?>upload/jasa/<?= $js['foto_jasa'];?>" alt="IMG-PRODUCT">
 
 								<div class="block2-overlay trans-0-4">
-									<a href="<?= base_url('produk');?>">
-										<img src="<?= base_url(); ?>assets/template/images/serum-purple1.jpg" alt="IMG-PRODUCT">
+									<a href="<?= base_url();?>produk/produk2/<?= $js['id_jasa'];?>">
+										<img src="<?= base_url(); ?>upload/jasa/<?= $js['foto_jasa'];?>" alt="IMG-PRODUCT">
 									</a>
 								</div>
 							</div>
 
 							<div class="block2-txt p-t-20">
-								<a href="<?= base_url('produk');?>" class="block2-name dis-block s-text3 p-b-5">
-									Serum Purple
+								<a href="<?= base_url();?>produk/produk2/<?= $js['id_jasa'];?>" class="block2-name dis-block s-text3 p-b-5">
+									<?= $js['nama_jasa'];?>
 								</a>
 
 								<span class="block2-price m-text6 p-r-5">
-									Rp 75.000
+									Rp.<?= $js['harga_jasa'];?>
 								</span>
 							</div>
 						</div>
 						<br>
-
-					<!-- block1 -->
-					<div class="block2">
-							<div class="block2-img hov-img-zoom wrap-pic-w of-hidden pos-relative">
-								<img src="<?= base_url(); ?>assets/template/images/Produk-Lenfi1.jpg
-								" alt="IMG-PRODUCT">
-
-								<div class="block2-overlay trans-0-4">
-									<a href="<?= base_url('produk');?>">
-										<img src="<?= base_url(); ?>assets/template/images/Produk-Lenfi1.jpg" alt="IMG-PRODUCT">
-									</a>
-								</div>
-							</div>
-
-							<div class="block2-txt p-t-20">
-								<a href="<?= base_url('produk/produk2');?>" class="block2-name dis-block s-text3 p-b-5">
-									Lenfi
-								</a>
-
-								<span class="block2-price m-text6 p-r-5">
-									Rp 92.500
-								</span>
-							</div>
-						</div>
-				</div>
-<br>
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="block2">
-							<div class="block2-img hov-img-zoom wrap-pic-w of-hidden pos-relative">
-								<img src="<?= base_url(); ?>assets/template/images/serum gold.jpg" alt="IMG-PRODUCT">
-
-								<div class="block2-overlay trans-0-4">
-									<a href="#" class="hov-pointer trans-0-4">
-										<img src="<?= base_url(); ?>assets/template/images/serum gold.jpg" alt="IMG-PRODUCT">
-									</a>
-								</div>
-							</div>
-
-							<div class="block2-txt p-t-20">
-								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Serum Gold
-								</a>
-
-								<span class="block2-price m-text6 p-r-5">
-									Rp 160.590
-								</span>
-							</div>
-						</div>
-
-						<br>
-					<div class="block2">
-							<div class="block2-img hov-img-zoom wrap-pic-w of-hidden pos-relative">
-								<img src="<?= base_url(); ?>assets/template/images/Sunscreen anak.jpg" alt="IMG-PRODUCT">
-
-								<div class="block2-overlay trans-0-4">
-									<a href="#" class="hov-pointer trans-0-4">
-										<img src="<?= base_url(); ?>assets/template/images/Sunscreen anak.jpg" alt="IMG-PRODUCT">
-									</a>
-								</div>
-							</div>
-
-							<div class="block2-txt p-t-20">
-								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Sunscreen anak
-								</a>
-
-								<span class="block2-price m-text6 p-r-5">
-									Rp 29.500
-								</span>
-							</div>
-						</div>
-				</div>
-				<br>
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="block2">
-							<div class="block2-img hov-img-zoom wrap-pic-w of-hidden pos-relative">
-								<img src="<?= base_url(); ?>assets/template/images/serum gold.jpg" alt="IMG-PRODUCT">
-
-								<div class="block2-overlay trans-0-4">
-									<a href="#" class="hov-pointer trans-0-4">
-										<img src="<?= base_url(); ?>assets/template/images/serum gold.jpg" alt="IMG-PRODUCT">
-									</a>
-								</div>
-							</div>
-
-							<div class="block2-txt p-t-20">
-								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Serum Gold
-								</a>
-
-								<span class="block2-price m-text6 p-r-5">
-									Rp 160.590
-								</span>
-							</div>
-						</div>
-
-						<br>
-					<div class="block2">
-							<div class="block2-img hov-img-zoom wrap-pic-w of-hidden pos-relative">
-								<img src="<?= base_url(); ?>assets/template/images/Sunscreen anak.jpg" alt="IMG-PRODUCT">
-
-								<div class="block2-overlay trans-0-4">
-									<a href="#" class="hov-pointer trans-0-4">
-										<img src="<?= base_url(); ?>assets/template/images/Sunscreen anak.jpg" alt="IMG-PRODUCT">
-									</a>
-								</div>
-							</div>
-
-							<div class="block2-txt p-t-20">
-								<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
-									Sunscreen anak
-								</a>
-
-								<span class="block2-price m-text6 p-r-5">
-									Rp 29.500
-								</span>
-							</div>
-						</div>
-				</div>
-
+					<?php endforeach;?>
 	</section>
