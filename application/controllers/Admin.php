ca312cea2ca837102a6e5c24700c2d98d4b42417
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-06 02:17:26 +0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,6 +7,8 @@ class Admin extends CI_Controller {
 		parent::__construct();
 		$this->load->model('Keamanan');
 		$this->load->model('Madmin');
+		$this->load->model('Produk');
+		$this->load->model('Rproduk');
 	}
 
 	//Halaman Utama Website	- Homepage
@@ -14,10 +16,18 @@ class Admin extends CI_Controller {
 	{
 		$this->Keamanan->getKeamananAdm();
 		$username = $this->session->userdata('username');
-
+		$data['ReservasiB'] = $this->Rproduk->jumlahRproduk1();
+		$data['ReservasiS'] = $this->Rproduk->jumlahRproduk2();
+		$all = $this->Produk->getAllProduk();
+		$jumlah = 0;
+		foreach ($all as $pr) {
+			$jumlah = $jumlah + $pr['jumlah_produk'];
+		}
+		$data['StokS'] = $jumlah;
+		$data['StokK'] = $this->Produk->jumlahStokKosong();
 		$isi['title'] = 'Klinik';
 		$this->load->view('admin/header', $isi);
-		$this->load->view('admin/content');
+		$this->load->view('admin/content',$data);
 		$this->load->view('admin/footer');
 	}
 
