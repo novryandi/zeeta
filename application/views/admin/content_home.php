8f6dedbfcf9aad5d9f049f9d926d1a8078e5b91f
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-05 23:35:27 +0700

    Update content_home.php

--- application/views/admin/content_home.php
+++ application/views/admin/content_home.php
@@ -1,4 +1,3 @@
-
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -17,27 +16,22 @@
             <!-- /.box-header -->
             <h3>Profil Klinik</h3>
             <!-- form start -->
-            <form role="form" method="post" action="#">
+            <form role="form" method="post" enctype="multipart/form-data" action="<?= base_url();?>AdminHome/index/<?= $home['id_home'];?>">
               <div class="box-body">
                 <div class="form-group">
                   <label>Judul</label>
-                  <input type="text" name="judul" class="form-control" placeholder="Judul" required>
+                  <input type="text" name="nama_home" id="nama_home" class="form-control" value="<?= $home['nama_home'];?>" required>
                 </div>
-
                 <div class="box-body pad">
-                  <form>
+                  <div class="form-group">
                     <label>Deskripsi</label>
-                    <textarea class="textarea" placeholder="Deskripsi Klinik"
-                              style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                  </form>
+                    <input type="text" name="deskripsi_home" id="deskripsi_home" class="form-control" value="<?= $home['deskripsi_home'];?>" required>
+                  </div>
                 </div>
-
         				<div class="form-group">
         					<label>Foto Profil Klinik</label>
-        					<input type="file" name="filefoto" class="dropify" data-height="300">
+        					<input type="file" name="foto_home" class="dropify" data-height="300">
         				</div>
-
-
               <!-- /.box-body -->
               <div class="box-footer">
                 <button type="submit" class="btn btn-primary" title="Simpan Data"> <i class="glyphicon glyphicon-floppy-disk"></i> Simpan</button>
