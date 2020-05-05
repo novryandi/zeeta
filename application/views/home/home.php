46f346a30045210afb6644f4548eca143a8dcc29
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-06 03:09:49 +0700

    Update home.php

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -2,113 +2,50 @@
 
 	<!--deskripsi salon-->
 	<div class="container" style="margin: 75px;">
-		<h2>Klinik Kecantikan Zeeta</h2>
+		<h2><?= $home['nama_home'];?></h2>
 			<div class="row">
 				<div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto">
 					<!-- block1 -->
 					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="<?= base_url('assets/template/images/zeeta banner.jpg'); ?> "alt="IMG-BENNER"> 
+						<img src="<?= base_url(); ?>upload/home/<?= $home['foto_home'];?> "alt="IMG-BENNER">
 					</div>
 				</div>
 
 				<div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto">
 					<!-- block1 -->
 					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<p>Kami membantu Anda mendapatkan tampilan wajah terbaik dengan menggabungkan seni dan ilmu estetika untuk menciptakan kecantikan dan keindahan wajah secara holistik dengan menggunakan metode inovatif dari sudut pandang estetika yang profesional dan ideal. Solusi yang Zeeta tawarkan adalah kombinasi cream program, perawatan medis oleh dokter profesional dan perawatan estetik oleh beauty therapist berpengalaman, didukung dengan cita rasa seni yang tinggi dan teknologi estetika yang canggih. </p>
+						<p><?= $home['deskripsi_home'];?></p>
 
 						<a href="<?= base_url('homepage/reservasi');?>" class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4"> Reservasi Sekarang</a>
 
-						
+
 					</div>
 
-					
+
 				</div>
 
 			</div>
 		</div>
-	
+
 	<!-- Banner -->
 	<section class="banner bgwhite p-t-40 p-b-40">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
 					<!-- block1 -->
+
+					<?php foreach($promo as $pr) :?>
 					<a href="#">
 					<div class="fit-image block1 hov-img-zoom-prm pos-relative m-b-30">
-						<img width="auto" height="300px" src="<?= base_url('assets/template/images/165610.jpg'); ?>" alt="IMG-BENNER">
+						<img width="auto" height="300px" src="<?= base_url(); ?>upload/promo/<?= $pr['foto_promo'];?>" alt="IMG-BENNER">
 
 						<div>
 							<!-- Button -->
 							<button class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Promo 1
+								<?= $pr['nama_promo'];?>
 							</button>
 							</a>
 						</div>
 					</div>
-
-					<!-- block1 -->
-					<div class="fit-image block1 hov-img-zoom-prm pos-relative m-b-30">
-						<img width="auto" height="300px" src="<?= base_url('assets/template/images/165611.jpg'); ?>"alt="IMG-BENNER">
-
-						<div>
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Promo 2
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="fit-image block1 hov-img-zoom-prm pos-relative m-b-30">
-						<img width="auto" height="300px" src="<?= base_url('assets/template/images/IMG-20200419-WA0010.jpg'); ?>" alt="IMG-BENNER">
-
-						<div>
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Promo 3
-							</a>
-						</div>
-					</div>
-
-					<!-- block1 -->
-					<div class="fit-image block1 hov-img-zoom-prm pos-relative m-b-30">
-						<img width="auto" height="300px" src="<?= base_url('assets/template/images/IMG-20200419-WA0012.jpg'); ?>" alt="IMG-BENNER">
-
-						<div>
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Promo 4
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="fit-image block1 hov-img-zoom-prm pos-relative m-b-30">
-						<img width="auto" height="300px" src="<?= base_url('assets/template/images/IMG-20200419-WA0009.jpg'); ?>"  alt="IMG-BENNER">
-						<div>
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4" width="auto">
-								Promo 5
-							</a>
-						</div>
-					</div>
-				
-					<!-- block1 -->
-					<div class="fit-image block1 hov-img-zoom-prm pos-relative m-b-30">
-						<img width="auto" height="300px" src="<?= base_url('assets/template/images/IMG-20200419-WA0011.jpg'); ?>" alt="IMG">
-
-							<div>
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Promo 6
-							</a>
-						</div>
-					</div>
-				</div>
-			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-
+				<?php endforeach;?>
 	</section>
