f40d40f77cf261611746975c2f0b98f784a1a350
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-06 03:14:34 +0700

    Update Produk.php

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -3,24 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Produk extends CI_Controller {
 
-	//Halaman Utama Website	- Homepage
-	public function index()
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Produk');
+		$this->load->model('Jasa');
+	}
+
+	public function index($id_produk)
 	{
-		$data =array( 'title'	=>	'Zeta Salon',
+		$data['produk'] = $this->Produk->getProdukById($id_produk);
+		$isi =array( 'title'	=>	'Zeta Salon',
 						'isi'	=>	'home/list');
 
-		$this->load->view('template/v_header');
+		$this->load->view('template/v_header',$isi);
 		$this->load->view('produk/produk1', $data);
 		$this->load->view('template/v_footer');
 
 	}
 
-	public function produk2()
+	public function produk2($id_jasa)
 	{
-		$data =array( 'title'	=>	'Zeta Salon',
+		$data['jasa'] = $this->Jasa->getJasaById($id_jasa);
+		$isi =array( 'title'	=>	'Zeta Salon',
 						'isi'	=>	'home/list');
 
-		$this->load->view('template/v_header');
+		$this->load->view('template/v_header',$isi);
 		$this->load->view('produk/produk2', $data);
 		$this->load->view('template/v_footer');
 
