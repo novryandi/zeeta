0d0a217234acac8dd984a987f86fcb2069a9d428
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-05 23:11:23 +0700

    Update PromoHome.php

--- application/controllers/PromoHome.php
+++ application/controllers/PromoHome.php
@@ -3,12 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class PromoHome extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Keamanan');
+		$this->load->model('Promo');
+	}
 
-	public function index()
+	public function index($id_promo)
 	{
+		$this->Keamanan->getKeamananAdm();
+
+		$this->form_validation->set_rules('nama_promo','Nama','required');
+		if($this->form_validation->run() == FALSE ){
+		$data['promo'] = $this->Promo->getPromoById($id_promo);
 		$isi['title'] = 'Klinik';
 		$this->load->view('admin/header', $isi);
-		$this->load->view('admin/editPromoHome');
+		$this->load->view('admin/editPromoHome',$data);
 		$this->load->view('admin/footer');
+	} else {
+		$this->Promo->ubahDataPromo($id_promo);
+		redirect('AdminHome/home_promo');
+	}
 	}
 }
