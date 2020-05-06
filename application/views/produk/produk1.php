c9413eaa99ccb279725437eaca04421b88b8a2d2
rosyidak <33874035+rosyidak@users.noreply.github.com>
2020-05-06 12:32:34 +0700

    Update produk1.php

--- application/views/produk/produk1.php
+++ application/views/produk/produk1.php
@@ -1,21 +1,21 @@
 <!-- //kalo bisa otomatis dari backend -->
-<h1><?$produk['nama_produk'];?></h1>
+<h1><?= $produk['nama_produk'];?></h1>
 
 <div class="container" style="margin: 75px;">
 	<div class="row">
 		<div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto">
 			<!-- block1 -->
 			<div class="block1 hov-img-zoom pos-relative m-b-30">
-				<img width="auto" height="500px" src="<?= base_url(); ?>upload/produk/<?$produk['foto'];?> "alt="IMG-BENNER">
+				<img width="auto" height="500px" src="<?= base_url(); ?>upload/produk/<?= $produk['foto'];?> "alt="IMG-BENNER">
 			</div>
 		</div>
 		<div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto">
 		<!-- block1 -->
 			<div class="block1 hov-img-zoom pos-relative m-b-30" >
-				<h2><?$produk['nama_produk'];?></h2><br>
-				<p><?$produk['deskripsi_produk'];?></p>
+				<h2<?= $produk['nama_produk'];?></h2><br>
+				<p><?= $produk['deskripsi'];?></p>
 				<br>
-				<h2 class="harga">Rp<?$produk['harga_produk'];?></h2>
+				<h2 class="harga">Rp.<?= $produk['harga_produk'];?></h2>
 				<!-- button -->
 				<a href="<?= base_url('homepage/reservasi');?>" class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4"> Reservasi Sekarang</a>
 			</div>
